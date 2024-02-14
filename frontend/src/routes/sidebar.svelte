70b3a890ccf95be5b4eb46553862beec9c6ec1d0
Zend <zendwang040302@outlook.com>
2024-02-14 05:26:45 +0000

    config

--- frontend/src/routes/sidebar.svelte
+++ frontend/src/routes/sidebar.svelte
@@ -15,7 +15,7 @@
 	// let darkmodebtn =
 	// 	'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5 fixed right-4 top-2 z-50';
 
-	const icons = [GridSolid, ArrowRightToBracketSolid, UserSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid];
+	const icons = [GridSolid, ArrowRightToBracketSolid, UserSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid, MailBoxSolid];
 </script>
 
 <!-- <DarkMode btnClass={darkmodebtn} /> -->
